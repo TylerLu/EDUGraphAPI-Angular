bffa572855ad355c5553524497fd942015f34885
TylerLu <admin@tyler.lu>
2017-01-03 23:06:07 +0800

    201701032306

--- src/EDUGraphAPI.Web/deploy.cmd
+++ src/EDUGraphAPI.Web/deploy.cmd
@@ -86,10 +86,11 @@ goto :EOF
 :: ----------
 
 :Deployment
-echo Handling node.js deployment.111
-
+echo Handling node.js deployment.222
+ 
 :: 1. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  echo %DEPLOYMENT_SOURCE%
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
