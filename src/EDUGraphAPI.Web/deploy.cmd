732eb5a1647e36fe16237950a53a0a8884a95148
TylerLu <admin@tyler.lu>
2017-01-04 13:11:07 +0800

    4

--- src/EDUGraphAPI.Web/deploy.cmd
+++ src/EDUGraphAPI.Web/deploy.cmd
@@ -21,10 +21,7 @@ IF %ERRORLEVEL% NEQ 0 (
 setlocal enabledelayedexpansion
 
 SET ARTIFACTS=%~dp0%..\artifacts
-
-IF NOT DEFINED DEPLOYMENT_SOURCE (
-  SET DEPLOYMENT_SOURCE=%~dp0%
-)
+SET DEPLOYMENT_SOURCE=%~dp0%\src\EDUGraphAPI.Web.
 
 IF NOT DEFINED DEPLOYMENT_TARGET (
   SET DEPLOYMENT_TARGET=%ARTIFACTS%\wwwroot
@@ -87,15 +84,13 @@ goto :EOF
 
 :Deployment
 echo Handling node.js deployment.
- 
+
 :: 1. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  echo %DEPLOYMENT_SOURCE%
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\src\EDUGraphAPI.Web" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  echo 666
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
-echo 777
+
 :: 2. Select node version
 call :SelectNodeVersion
 
