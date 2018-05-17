bef6582177c46df595e7d251d1299ff083081566
Tyler Lu <tyler.lu@canviz.com>
2018-05-17 10:22:39 +0800

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,11 +1,7 @@
 :: Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 :: See LICENSE in the project root for license information.
 
-
-echo %cd%
-
 IF EXIST "%~dp0%\src\EDUGraphAPI.Web\deploy.cmd" (
-  pushd "%~dp0%\src\EDUGraphAPI.Web%"
-  call deploy.cmd
+  call %~dp0%\src\EDUGraphAPI.Web\deploy.cmd
   popd
 )
